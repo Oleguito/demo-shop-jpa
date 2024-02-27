f6af1006f32057f995148187758c9eb3199b0c15
Oleguito <tenzoriator@rambler.ru>
2024-02-27 17:32:04 +0300

    all tests passed Tue 02/27/2024 17:32:04.88

---
+++ src/main/java/com/example/presentation/purchase/PurchaseController.java
@@ -0,0 +1,15 @@
+package com.example.presentation.purchase;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/purchases")
+public class PurchaseController {
+
+
+
+}
---
+++ src/test/java/com/example/PurchaseControllerTests.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import com.example.domain.entity.Purchase;
+import com.example.presentation.purchase.PurchaseController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(classes = ShopApplication.class)
+public class PurchaseControllerTests {
+    
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private PurchaseController purchaseController;
+    
+    private ObjectMapper jackson;
+    
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(purchaseController).build();
+    }
+    
+    @Test
+    void createAPurchase() throws Exception {
+        final var purchase = new Purchase();
+        assertTrue(purchase != null);
+    }
+    
+    @Test
+    void buildAPurchase() throws Exception {
+        final var purchase = Purchase.builder().build();
+        assertTrue(purchase != null);
+    }
+    
+    @Test
+    void listAllPurchasesByUser() throws Exception {
+    
+    }
+}
